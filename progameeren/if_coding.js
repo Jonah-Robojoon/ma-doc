19d91bae11dce46ffc31035bdfe3fb51d182e26a
Jonah <38752@ma-web.nl>
2024-09-25 17:09:14 +0200

    fox

--- progameeren/if_coding.js
+++ progameeren/if_coding.js
@@ -1,6 +1,6 @@
-const huidigeDag= "Woensdag";
+var huidigeDag = new Date().getDay;
 
-if(huidigeDag == "Maandag" || huidigeDag == "Dinsdag" || huidigeDag == "Woensdag" || huidigeDag == "Dinsdag" || huidigeDag == "Vrijdag"){  
+if(huidigeDag == "1" || huidigeDag == "2" || huidigeDag == "3" || huidigeDag == "4" || huidigeDag == "5"){  
     console.log("Ik moet naar school");
 } else {
     console.log("Lekker uitslapen!");
