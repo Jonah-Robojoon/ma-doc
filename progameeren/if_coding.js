8ffa517fbe6c80644ae87e8c00e8c8dd68c0e363
Jonah <38752@ma-web.nl>
2024-09-25 17:23:24 +0200

    fix

--- progameeren/if_coding.js
+++ progameeren/if_coding.js
@@ -1,6 +1,6 @@
 var huidigeDag = new Date().getDay;
 
-if(huidigeDag == "1" || huidigeDag == "2" || huidigeDag == "3" || huidigeDag == "4" || huidigeDag == "5"){  
+if(huidigeDag > 0 && huidigeDag < 6)S{  
     console.log("Ik moet naar school");
 } else {
     console.log("Lekker uitslapen!");
