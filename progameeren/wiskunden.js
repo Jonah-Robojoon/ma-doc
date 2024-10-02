8e943379afeb2062fd236eb3754e404d880e9ac8
Jonah <38752@ma-web.nl>
2024-10-02 15:41:30 +0200

    1.0

---
+++ progameeren/wiskunden.js
@@ -0,0 +1,39 @@
+//berekent de inhoud van kubus
+function berekenInhoud(lenkte,breede,hoogte){
+    let Inhoud = lenkte * breede * hoogte;
+    return Inhoud;
+}
+
+let kubusInhoud = berekenInhoud(10, 5, 10);
+console.log(kubusInhoud);
+
+
+//berekent de inhoud van cylinder
+function berekenCylinderInhoud(Radius, hoogte){
+    let Inhoud = Radius**2 * Math.PI * hoogte;
+    return Inhoud;
+}
+
+let CylinderInhoud = berekenCylinderInhoud(10, 5);
+console.log(CylinderInhoud);
+
+
+//berekend de zijde van een driehoek
+function berekenDriehoekZijde(lengte, hoogte){
+    let Zijde = Math.sqrt(lengte**2 + hoogte**2);
+    return Zijde;
+}
+
+let DriehoekZijde = berekenDriehoekZijde(10, 5);
+console.log(DriehoekZijde);
+
+
+//berkend gemiddelde nummer
+function gemiddelde(G1,G2,G3,G4,G5,G6,G7){
+    let gemiddeldeSijfer = (G1+G2+G3+G4+G5+G6+G7)/7;
+    return gemiddeldeSijfer;
+}
+
+let gemiddeldeNummer = gemiddelde(5,8,3,4,1,9,4);
+console.log(gemiddeldeNummer)
+
